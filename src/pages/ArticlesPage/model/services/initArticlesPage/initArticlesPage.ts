238c992b5006073f6fac5661801890b055582c9c
HRKNK <nikita.zyamzin@yandex.ru>
2023-04-02 14:15:56 +0300

    update: fixed init store for articles

--- src/pages/ArticlesPage/model/services/initArticlesPage/initArticlesPage.ts
+++ src/pages/ArticlesPage/model/services/initArticlesPage/initArticlesPage.ts
@@ -5,16 +5,14 @@ import { articlesPageActions } from '../../slices/articlesPageSlice';
 import { fetchArticlesList } from '../fetchArticlesList/fetchArticlesList';
 
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import { type ThunkConfig } from 'app/providers/StoreProvider/public';
-import { useSelector } from 'react-redux';
+import { type StateSchema, type ThunkConfig } from 'app/providers/StoreProvider/public';
 
 // eslint-disable-next-line @typescript-eslint/no-invalid-void-type
 export const initArticlesPage = createAsyncThunk<void, void, ThunkConfig<string>>(
 	'articlesPage/initArticlesPage',
 	async (_, thunkApi) => {
-		const { dispatch } = thunkApi;
-		const inited = useSelector(getArticlesPageInited);
-
+		const { getState, dispatch } = thunkApi;
+		const inited = getArticlesPageInited(getState() as StateSchema);
 		if (!inited) {
 			dispatch(articlesPageActions.initState()); // достаем текущий вид отображения страницы
 			void dispatch(fetchArticlesList({ page: 1 })); // запрос на статьи | номер страницы
