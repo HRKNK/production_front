8ea9865db54da546cefc1008f8de1c5cbe806c82
HRKNK <nikita.zyamzin@yandex.ru>
2023-04-02 14:35:39 +0300

    update: fix undefined text value

--- src/features/addCommentForm/model/selectors/addCommentFormSelectors.ts
+++ src/features/addCommentForm/model/selectors/addCommentFormSelectors.ts
@@ -1,5 +1,5 @@
 import { type StateSchema } from 'app/providers/storeProvider/public';
 
 // Дополнить StateSchema
-export const getAddCommentFormText = (state: StateSchema) => state.addCommentForm?.text;
+export const getAddCommentFormText = (state: StateSchema) => state.addCommentForm?.text ?? '';
 export const getAddCommentFormError = (state: StateSchema) => state.addCommentForm?.error;
