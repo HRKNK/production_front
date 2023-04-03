b78d9c9749ea74c8cc32457e4c6bef90ed9c2ab5
HRKNK <nikita.zyamzin@yandex.ru>
2023-04-03 17:33:39 +0300

    added: Debounce hook

---
+++ src/shared/lib/hooks/useDebounce/useDebounce.ts
@@ -0,0 +1,17 @@
+import { type MutableRefObject, useCallback, useRef } from 'react';
+
+// Запуск события через задержку (отложенный запуск)
+export function useDebounce (callback: (...args: any[]) => void, delay: number) {
+	// eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
+	const timer = useRef() as MutableRefObject<any>;
+
+	return useCallback((...args: any[]) => {
+		if (timer.current) { // если таймер уже активен
+			clearTimeout(timer.current); // очищаем
+		}
+		timer.current = setTimeout(() => { // запускаем новый таймер отложенного запуска
+			// eslint-disable-next-line n/no-callback-literal
+			callback(...args);
+		}, delay);
+	}, [callback, delay]);
+}
