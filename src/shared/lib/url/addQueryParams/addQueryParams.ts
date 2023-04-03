4a5cb4bf913ad4ff956a499482cb885a45ec3dd5
HRKNK <nikita.zyamzin@yandex.ru>
2023-04-03 17:34:15 +0300

    added: query helpers

---
+++ src/shared/lib/url/addQueryParams/addQueryParams.ts
@@ -0,0 +1,15 @@
+export function getQueryParams (params: Record<string, string>) {
+	const searchParams = new URLSearchParams(window.location.search);
+	Object.entries(params).forEach(([name, value]) => {
+		if (value !== undefined) {
+			searchParams.set(name, value);
+		}
+	});
+	// строка параметров (квери)
+	return `?${searchParams.toString()}`;
+}
+
+//  Функция добавления параметров строки запроса в URL
+export function addQueryParams (params: Record<string, string>) {
+	window.history.pushState(null, '', getQueryParams(params));
+}
