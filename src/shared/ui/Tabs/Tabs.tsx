ca9e3aea588b3329d604dbc5edba5e8728ad1675
HRKNK <nikita.zyamzin@yandex.ru>
2023-04-03 19:22:51 +0300

    added: tab component

---
+++ src/shared/ui/Tabs/Tabs.tsx
@@ -0,0 +1,43 @@
+import cls from './Tabs.module.scss';
+
+import classNames from 'shared/lib/classNames/classNames';
+import { memo, type ReactNode, useCallback } from 'react';
+import { Card, CardTheme } from 'shared/ui/Card/Card';
+
+export interface TabItem {
+	value: string;
+	content: ReactNode;
+}
+
+interface TabsProps {
+	className?: string;
+	tabs: TabItem[]; // массив табов
+	value: string; // выбранное значение
+	onTabClick: (tab: TabItem) => void; // переключение табов
+}
+
+// eslint-disable-next-line react/display-name
+export const Tabs = memo((props: TabsProps) => {
+	const { className, tabs, onTabClick, value } = props;
+
+	const clickHandle = useCallback((tab: TabItem) => () => { // механизм замыкания пробросит таб
+		onTabClick(tab);
+	}, [onTabClick]);
+
+	return (
+		<div className={classNames(cls.Tabs, {}, [className])}>
+			{tabs.map((tab) => (
+				<Card
+					// если значение текущего таба === выбранному значению
+					theme={tab.value === value ? CardTheme.NORMAL : CardTheme.OUTLINED}
+					className={cls.tab}
+					key={tab.value}
+					// внутрь передается сам таб
+					onClick={clickHandle(tab)}
+				>
+					{tab.content}
+				</Card>
+			))}
+		</div>
+	);
+});
