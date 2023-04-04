6f45a6b3a975ec59719ab16cfb9e92eb91899f8d
HRKNK <nikita.zyamzin@yandex.ru>
2023-04-04 16:21:27 +0300

    added: set Q params

--- src/pages/ArticlesPage/model/services/initArticlesPage/initArticlesPage.ts
+++ src/pages/ArticlesPage/model/services/initArticlesPage/initArticlesPage.ts
@@ -6,14 +6,23 @@ import { fetchArticlesList } from '../fetchArticlesList/fetchArticlesList';
 
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { type StateSchema, type ThunkConfig } from 'app/providers/StoreProvider/public';
+import { type ArticleType, type ArticleSortField } from 'entities/Article/public';
+import { type SortOrder } from 'shared/types';
 
 // eslint-disable-next-line @typescript-eslint/no-invalid-void-type
-export const initArticlesPage = createAsyncThunk<void, void, ThunkConfig<string>>(
+export const initArticlesPage = createAsyncThunk<void, URLSearchParams, ThunkConfig<string>>(
+	// createAsyncThunk<(что возвращаем), (что ожидаем на вход), { переопределение типа }
 	'articlesPage/initArticlesPage',
-	async (_, thunkApi) => {
+	async (searchParams, thunkApi) => { // searchParams: URLSearchParams = принят на вход createAsyncThunk
 		const { getState, dispatch } = thunkApi;
 		const inited = getArticlesPageInited(getState() as StateSchema);
 		if (!inited) {
+			// записываем квери параметры из url строки
+			dispatch(articlesPageActions.setOrder(searchParams.get('order') as SortOrder ?? null));
+			dispatch(articlesPageActions.setSort(searchParams.get('sort') as ArticleSortField ?? null));
+			dispatch(articlesPageActions.setSearch(searchParams.get('search') ?? null));
+			dispatch(articlesPageActions.setType(searchParams.get('type') as ArticleType ?? null));
+
 			dispatch(articlesPageActions.initState()); // достаем текущий вид отображения страницы
 			void dispatch(fetchArticlesList({})); // запрос на статьи | номер страницы  page: 1
 		}
