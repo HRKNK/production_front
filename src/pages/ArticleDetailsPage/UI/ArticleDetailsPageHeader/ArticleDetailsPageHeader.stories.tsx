71c9dfd6c02449c28f5588181c9dcd064761b1c5
HRKNK <nikita.zyamzin@yandex.ru>
2023-04-05 20:01:23 +0300

    added: ArticlesPage Headers

---
+++ src/pages/ArticleDetailsPage/UI/ArticleDetailsPageHeader/ArticleDetailsPageHeader.stories.tsx
@@ -0,0 +1,18 @@
+import { ArticleDetailsPageHeader } from './ArticleDetailsPageHeader';
+
+import React from 'react';
+import { type ComponentStory, type ComponentMeta } from '@storybook/react';
+
+// eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+export default {
+	title: 'shared/ArticleDetailsPageHeader',
+	component: ArticleDetailsPageHeader,
+	argTypes: {
+		backgroundColor: { control: 'color' },
+	},
+} as ComponentMeta<typeof ArticleDetailsPageHeader>;
+
+const Template: ComponentStory<typeof ArticleDetailsPageHeader> = (args) => <ArticleDetailsPageHeader {...args} />;
+
+export const Normal = Template.bind({});
+Normal.args = {};
---
+++ src/pages/ArticleDetailsPage/UI/ArticleDetailsPageHeader/ArticleDetailsPageHeader.tsx
@@ -0,0 +1,47 @@
+import cls from './ArticleDetailsPageHeader.module.scss';
+
+import { getCanEditArticle } from '../../model/selectors/article';
+
+import classNames from 'shared/lib/classNames/classNames';
+import { useTranslation } from 'react-i18next';
+import { memo, useCallback } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { RoutePath } from 'shared/config/routeConfig';
+import { useSelector } from 'react-redux';
+// import { getUserAuthData } from 'entities/User/public';
+import Button, { ThemeButton } from 'shared/ui/Button/Button';
+import { getArticleDetailsData } from 'entities/Article/public';
+
+interface ArticleDetailsPageHeaderProps {
+	className?: string;
+}
+
+// eslint-disable-next-line react/display-name
+export const ArticleDetailsPageHeader = memo((props: ArticleDetailsPageHeaderProps) => {
+	const { className } = props;
+	const { t } = useTranslation();
+	const navigate = useNavigate();
+	const canEdit = useSelector(getCanEditArticle); // проверка на article.user.id === user.id
+	const article = useSelector(getArticleDetailsData);
+
+	const onBackToList = useCallback(() => { // возврат обратно к списку
+		navigate(RoutePath.articles);
+	}, [navigate]);
+
+	const onEditArticle = useCallback(() => {
+		navigate(`${RoutePath.articles_details}${article?.id}/edit`); // маршрут на страницу изменения
+	}, [article?.id, navigate]);
+
+	return (
+		<div className={classNames(cls.ArticleDetailsPageHeader, {}, [className])}>
+			<Button theme={ThemeButton.OUTLINE} onClick={onBackToList}>
+				{t('Назад к списку')}
+			</Button>
+			{canEdit && (
+				<Button className={cls.editBtn} theme={ThemeButton.OUTLINE} onClick={onEditArticle}>
+					{t('Редактировать')}
+				</Button>
+			)}
+		</div>
+	);
+});
