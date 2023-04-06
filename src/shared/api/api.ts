6e781973a80e0b4269a141cac32728c6d4b3a4af
HRKNK <nikita.zyamzin@yandex.ru>
2023-04-06 18:45:47 +0300

    fix: added interceptors

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -4,7 +4,15 @@ import { USER_LOCALSTORAGE_KEY } from 'shared/const/localstorage';
 export const $api = axios.create({
 	// baseURL: 'http://localhost:8000',
 	baseURL: _API,
-	headers: {
-		authorization: localStorage.getItem(USER_LOCALSTORAGE_KEY) || '',
-	},
+	// headers: {
+	// 	authorization: localStorage.getItem(USER_LOCALSTORAGE_KEY) || '',
+	// },
+});
+
+// interceptor - перехватчик. вешается на response, request
+$api.interceptors.request.use((config) => {
+	if (config.headers) { // Сделайте что-нибудь перед отправкой запроса ->
+		config.headers.Authorization = localStorage.getItem(USER_LOCALSTORAGE_KEY) || '';
+	}
+	return config;
 });
