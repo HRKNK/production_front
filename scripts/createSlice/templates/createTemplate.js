863707642f2025885e51ed5b989d494d7c185fbf
HRKNK <nikita.zyamzin@yandex.ru>
2023-04-13 17:37:36 +0300

    added: entity generator

---
+++ scripts/createSlice/templates/createTemplate.js
@@ -0,0 +1,19 @@
+const createModel = require('./createModel');
+const createUI = require('./createUI');
+const createPublicApi = require('./createPublicApi');
+
+const resolveRoot = require('../resolveRoot');
+
+const fs = require('fs/promises');
+
+module.exports = async (layer, sliceName) => {
+	try {
+		await fs.mkdir(resolveRoot('src', layer, sliceName));
+	} catch (e) {
+		console.log(`не удалось создать директорию для слайса${sliceName}`);
+	}
+
+	await createModel(layer, sliceName);
+	await createUI(layer, sliceName);
+	await createPublicApi(layer, sliceName);
+};
