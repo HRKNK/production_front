a7d0d1403a222a8bf07e086bb30924198e87f17e
HRKNK <nikita.zyamzin@yandex.ru>
2023-04-14 19:17:00 +0300

    update

--- src/features/articleRecommendationsList/api/aritcleRecommendationsApi.ts
+++ src/features/articleRecommendationsList/api/aritcleRecommendationsApi.ts
@@ -1,8 +1,10 @@
+import { type Article } from 'entities/Article/public';
 import { rtkApi } from 'shared/api/rtkApi';
 
 const recommendationsApi = rtkApi.injectEndpoints({
 	endpoints: (build) => ({
-		getArticleRecommendationsList: build.query({
+		getArticleRecommendationsList: build.query<Article[], number>({
+			// build.query < (что возвращаем), (что ожидаем на вход) >
 			query: (limit) => ({
 				url: '/articles',
 				params: {
