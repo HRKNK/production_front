e452e16a4536d2d053edc0f2704ce24cc289d77e
HRKNK <nikita.zyamzin@yandex.ru>
2023-04-16 17:25:00 +0300

    update

--- src/shared/lib/hooks/useInitialEffect/useInitialEffect.ts
+++ src/shared/lib/hooks/useInitialEffect/useInitialEffect.ts
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 
 export function useInitialEffect (callback: () => void) {
 	useEffect(() => {
-		if (_PROJECT !== 'storybook') {
+		if (_PROJECT !== 'storybook' && _PROJECT !== 'jest') { // проверка на тестирование (отмена эффекта лоадера)
 			callback();
 		}
 		// eslint-disable-next-line
