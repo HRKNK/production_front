b457d19ff7eece2cc6d0177230cbefd1afe5c3c2
HRKNK <nikita.zyamzin@yandex.ru>
2023-04-24 15:15:35 +0300

    update: added props inverted

--- src/shared/ui/Icon/Icon.tsx
+++ src/shared/ui/Icon/Icon.tsx
@@ -1,18 +1,19 @@
 import cls from './Icon.module.scss';
 
 import classNames from 'shared/lib/classNames/classNames';
-import React, { memo } from 'react';
+import React, { type VFC, memo } from 'react';
 
 interface IconProps {
 	className?: string;
 	Svg: React.VFC<React.SVGProps<SVGSVGElement>>;
+	inverted?: boolean;
 }
 
 // eslint-disable-next-line react/display-name
 export const Icon = memo((props: IconProps) => {
-	const { className, Svg } = props;
+	const { className, Svg, inverted } = props;
 
 	return (
-		<Svg className={classNames(cls.Icon, {}, [className])} />
+		<Svg className={classNames(inverted ? cls.inverted : cls.Icon, {}, [className])} />
 	);
 });
