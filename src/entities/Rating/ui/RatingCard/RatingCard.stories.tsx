9498e1ec7aabc606b7fae326be73503241eff83d
HRKNK <nikita.zyamzin@yandex.ru>
2023-04-28 12:07:43 +0300

    added: Rating entities

---
+++ src/entities/Rating/ui/RatingCard/RatingCard.stories.tsx
@@ -0,0 +1,18 @@
+import { RatingCard } from './RatingCard';
+
+import React from 'react';
+import { type ComponentStory, type ComponentMeta } from '@storybook/react';
+
+// eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+export default {
+	title: 'shared/RatingCard',
+	component: RatingCard,
+	argTypes: {
+		backgroundColor: { control: 'color' },
+	},
+} as ComponentMeta<typeof RatingCard>;
+
+const Template: ComponentStory<typeof RatingCard> = (args) => <RatingCard {...args} />;
+
+export const Normal = Template.bind({});
+Normal.args = {};
---
+++ src/entities/Rating/ui/RatingCard/RatingCard.tsx
@@ -0,0 +1,98 @@
+import { useTranslation } from 'react-i18next';
+import { memo, useCallback, useState } from 'react';
+
+import classNames from 'shared/lib/classNames/classNames';
+import { Card } from 'shared/ui/Card/Card';
+import { HStack, VStack } from 'shared/ui/Stack/public';
+import { Text } from 'shared/ui/Text/public';
+import { StarRating } from 'shared/ui/StarRating/StarRating';
+import { Modal } from 'shared/ui/Modal/public';
+import { Input } from 'shared/ui/Input/public';
+import Button, { ButtonSize, ThemeButton } from 'shared/ui/Button/Button';
+import { Drawer } from 'shared/ui/Drawer/Drawer';
+import { useDevice } from 'shared/lib/hooks/useDevice/useDevice';
+
+interface RatingCardProps {
+	className?: string;
+	title?: string; // заголовок над звездами
+	feedbackTitle?: string; // заголовок отзыва(фидбек)
+	hasFeedback?: boolean; // оставить отзыв (фидбек)?
+	onCancel?: (starsCount: number) => void; // отмена отправки
+	onAccept?: (starsCount: number, feedback?: string) => void; // отправить отзыв
+}
+
+// eslint-disable-next-line react/display-name
+export const RatingCard = memo((props: RatingCardProps) => {
+	const { className, onAccept, feedbackTitle, hasFeedback, onCancel, title } = props;
+	const { t } = useTranslation();
+	const [isModalOpen, setIsModalOpen] = useState(false); // состояние модалки
+	const [starsCount, setStarsCount] = useState(0);
+	const [feedback, setFeedback] = useState(''); // связывание с инпутом
+
+	const onSelectStars = useCallback((selectedStarsCount: number) => {
+		setStarsCount(selectedStarsCount);
+		if (hasFeedback) {
+			setIsModalOpen(true);
+		} else {
+			onAccept?.(selectedStarsCount);
+		}
+	}, [hasFeedback, onAccept]);
+
+	const acceptHandle = useCallback(() => {
+		setIsModalOpen(false);
+		onAccept?.(starsCount, feedback);
+	}, [feedback, onAccept, starsCount]);
+
+	const cancelHandle = useCallback(() => {
+		setIsModalOpen(false);
+		onCancel?.(starsCount);
+	}, [onCancel, starsCount]);
+
+	const isMobileView = useDevice(); // вид мобильного варианта
+
+	const modalContent = (
+		<>
+			<Text title={feedbackTitle}/>
+			<Input value={feedback} onChange={setFeedback} placeholder={t('Ваш отзыв')}/>
+		</>
+	);
+
+	return (
+		<Card className={classNames('', {}, [className])}>
+			<VStack align='center' gap='8'>
+				<Text title={title} />
+				{/* Компонент звезд */}
+				<StarRating size={40} onSelect={onSelectStars} />
+			</VStack>
+
+			{isMobileView
+				? <>
+					{/* Мобильная версия */}
+					<Drawer isOpen={isModalOpen} // lazy
+						onClose={cancelHandle}>
+						<VStack gap='32'>
+							{modalContent}
+							<Button // fullWidth
+								onClick={acceptHandle} size={ButtonSize.L}>
+								{t('Отправить')}
+							</Button>
+						</VStack>
+					</Drawer>
+				</>
+				: <Modal isOpen={isModalOpen} lazy> {/* Декстопная версия */}
+					<VStack max gap='32'>
+						{modalContent}
+						<HStack max gap='16' justify='end'>
+							<Button onClick={cancelHandle} theme={ThemeButton.OUTLINE_RED}>
+								{t('Закрыть')}
+							</Button>
+							<Button onClick={acceptHandle}>
+								{t('Отправить')}
+							</Button>
+						</HStack>
+					</VStack>
+				</Modal>
+			}
+		</Card>
+	);
+});
