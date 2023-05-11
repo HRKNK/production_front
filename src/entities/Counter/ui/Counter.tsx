ca812524825e2284f3360cb6703ba6c0dcf4f9a9
HRKNK <nikita.zyamzin@yandex.ru>
2023-05-11 13:56:43 +0300

    update: eslint custom rule

--- src/entities/Counter/ui/Counter.tsx
+++ src/entities/Counter/ui/Counter.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
-import { type StateSchema } from 'app/providers/storeProvider/config/stateSchema';
-
 import { getCounterValue } from '../model/selectors/getCounterValue/getCounterValue';
 import { counterActions } from '../model/slice/counterSlice';
 
