65ca4aeb06ad389655777fc4269bda23bd4479b4
HRKNK <nikita.zyamzin@yandex.ru>
2023-05-22 19:42:13 +0300

    update: minor

--- src/shared/lib/features/setGetFeatures.ts
+++ src/shared/lib/features/setGetFeatures.ts
@@ -1,6 +1,6 @@
 import { type FeatureFlags } from 'shared/types/featureFlags';
 
-let featureFlags: FeatureFlags;
+let featureFlags: FeatureFlags = {};
 
 // Кладем текущий флаг
 export function setFeatureFlags(newFeatureFlags?: FeatureFlags) {
@@ -11,5 +11,5 @@ export function setFeatureFlags(newFeatureFlags?: FeatureFlags) {
 
 // Получаем текущий флаг
 export function getFeatureFlag(flag: keyof FeatureFlags) {
-	return featureFlags[flag];
+	return featureFlags?.[flag];
 }
