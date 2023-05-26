1889cb60c165fbd554bb3377affc686107c1aee9
HRKNK <nikita.zyamzin@yandex.ru>
2023-05-26 15:49:34 +0300

    update: minor

--- src/widgets/SideBar/model/types/items.ts
+++ src/widgets/SideBar/model/types/items.ts
@@ -2,7 +2,7 @@ import type React from 'react';
 
 export interface SidebarItemType {
 	path: string;
-	Icon: React.VFC<React.SVGProps<SVGSVGElement>>; // SVG type из глобал тс
+	Icon: React.FC<React.SVGProps<SVGSVGElement>>; // SVG type из глобал тс
 	text: string;
 	authOnly?: boolean;
 }
