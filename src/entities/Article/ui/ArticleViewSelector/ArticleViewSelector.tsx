34ca9ab9895be9ebbd5caed370c8965d6e2298a9
HRKNK <nikita.zyamzin@yandex.ru>
2023-05-27 21:25:58 +0300

    update: flag redesign added

--- src/entities/Article/ui/ArticleViewSelector/ArticleViewSelector.tsx
+++ src/entities/Article/ui/ArticleViewSelector/ArticleViewSelector.tsx
@@ -1,10 +1,17 @@
 import { memo } from 'react';
 
-import ListIcon from 'shared/assets/icons/list.svg';
-import TiledIcon from 'shared/assets/icons/tiled.svg';
+import ListIcon from 'shared/assets/icons/burger.svg';
+import ListIconDeprecated from 'shared/assets/icons/list.svg';
+import TiledIcon from 'shared/assets/icons/tile.svg';
+import TiledIconDeprecated from 'shared/assets/icons/tiled.svg';
 import classNames from 'shared/lib/classNames/classNames';
-import { Button, ThemeButton } from 'shared/ui/deprecated/Button/public';
-import { Icon } from 'shared/ui/deprecated/Icon/public';
+import { ToggleFeatures } from 'shared/lib/features/public';
+import { toggleFeatures } from 'shared/lib/features/toggleFeatures';
+import { Button as ButtonDeprecated, ThemeButton } from 'shared/ui/deprecated/Button/public';
+import { Icon as IconDeprecated } from 'shared/ui/deprecated/Icon/public';
+import { Card } from 'shared/ui/redesigned/Card/public';
+import { Icon } from 'shared/ui/redesigned/Icon/public';
+import { HStack } from 'shared/ui/redesigned/Stack/public';
 
 import { ArticleView } from '../../model/consts/consts';
 import cls from './ArticleViewSelector.module.scss';
@@ -18,11 +25,19 @@ interface ArticleViewSelectorProps {
 const viewTypes = [
 	{
 		view: ArticleView.SMALL,
-		icon: TiledIcon,
+		icon: toggleFeatures({
+			name: 'isAppRedesigned',
+			on: () => TiledIcon,
+			off: () => TiledIconDeprecated,
+		}),
 	},
 	{
 		view: ArticleView.BIG,
-		icon: ListIcon,
+		icon: toggleFeatures({
+			name: 'isAppRedesigned',
+			on: () => ListIcon,
+			off: () => ListIconDeprecated,
+		}),
 	},
 ];
 
@@ -35,12 +50,32 @@ export const ArticleViewSelector = memo((props: ArticleViewSelectorProps) => {
 	};
 
 	return (
-		<div className={classNames(cls.ArticleViewSelector, {}, [className])}>
-			{viewTypes.map((viewType) => (
-				<Button key={viewType.view} theme={ThemeButton.CLEAR} onClick={onClick(viewType.view)}>
-					<Icon width={24} height={24} Svg={viewType.icon} className={classNames('', { [cls.notSelected]: viewType.view !== view })} />
-				</Button>
-			))}
-		</div>
+		<ToggleFeatures
+			feature={'isAppRedesigned'}
+			on={
+				<Card border="border_round" className={classNames(cls.ArticleViewSelectorRedesigned, {}, [className])}>
+					{/* <HStack gap="8"> */}
+					{viewTypes.map((viewType) => (
+						<Icon
+							key={viewType.view}
+							clickable
+							onClick={onClick(viewType.view)}
+							Svg={viewType.icon}
+							className={classNames('', { [cls.notSelected]: viewType.view !== view })}
+						/>
+					))}
+					{/* </HStack> */}
+				</Card>
+			}
+			off={
+				<div className={classNames(cls.ArticleViewSelector, {}, [className])}>
+					{viewTypes.map((viewType) => (
+						<ButtonDeprecated key={viewType.view} theme={ThemeButton.CLEAR} onClick={onClick(viewType.view)}>
+							<IconDeprecated width={24} height={24} Svg={viewType.icon} className={classNames('', { [cls.notSelected]: viewType.view !== view })} />
+						</ButtonDeprecated>
+					))}
+				</div>
+			}
+		/>
 	);
 });
