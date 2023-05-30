658d623dbd7496a4cbdf0efff5ab1e55eaee5ffc
HRKNK <nikita.zyamzin@yandex.ru>
2023-05-30 21:12:32 +0300

    update: minor style fix (design)

--- src/shared/ui/redesigned/Text/ui/Text.tsx
+++ src/shared/ui/redesigned/Text/ui/Text.tsx
@@ -16,6 +16,7 @@ interface TextProps {
 	variant?: TextVariant;
 	align?: TextAlign;
 	size?: TextSize;
+	bold?: boolean; // жирный?
 	'data-testid'?: string;
 }
 
@@ -33,7 +34,7 @@ const mapSizeToClass: Record<TextSize, string> = {
 };
 
 export const Text = memo((props: TextProps) => {
-	const { className, text, title, variant = 'primary', align = 'left', size = 'm', 'data-testid': dataTestId = 'Text' } = props;
+	const { className, text, title, bold, variant = 'primary', align = 'left', size = 'm', 'data-testid': dataTestId = 'Text' } = props;
 
 	const HeaderTag = mapSizeToHeaderTag[size]; // по размеру достаем текущий тэг // h1 | h2 | h3
 	const sizeClass = mapSizeToClass[size];
@@ -41,7 +42,7 @@ export const Text = memo((props: TextProps) => {
 	const additionalClasses = [className, cls[variant], cls[align], sizeClass];
 
 	return (
-		<div className={classNames(cls.Text, {}, additionalClasses)}>
+		<div className={classNames(cls.Text, { [cls.bold]: bold }, additionalClasses)}>
 			{title && (
 				<HeaderTag className={cls.title} data-testid={`${dataTestId}.Header`}>
 					{title}
