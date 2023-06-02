db852aa4245574f2b271fc9d5e32ff7c56ae7b1b
HRKNK <nikita.zyamzin@yandex.ru>
2023-06-02 18:23:53 +0300

    update: added color type

--- src/shared/ui/redesigned/Button/Button.tsx
+++ src/shared/ui/redesigned/Button/Button.tsx
@@ -6,6 +6,7 @@ import classNames, { type Mods } from 'shared/lib/classNames/classNames';
 import cls from './Button.module.scss';
 
 export type ButtonVariant = 'clear' | 'outline' | 'filled';
+export type ButtonColor = 'normal' | 'success' | 'error';
 export type ButtonSize = 'm' | 'l' | 'xl';
 
 // наследуем атрибуты кнопки
@@ -17,13 +18,26 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 	disabled?: boolean; // disabled?
 	children?: ReactNode; // ReactNode
 	fullWidth?: boolean; // Растянуть на всю ширину
+	color?: ButtonColor; // Цвет кнопки
 
 	addonLeft?: ReactNode; // Дополнение слева (иконка)
 	addonRight?: ReactNode; // Дополнение справа (иконка)
 }
 
 export const Button = memo((props: ButtonProps) => {
-	const { className, children, addonLeft, addonRight, variant = 'outline', square, disabled, fullWidth, size = 'm', ...otherProps } = props;
+	const {
+		className,
+		children,
+		addonLeft,
+		addonRight,
+		variant = 'outline',
+		color = 'normal',
+		square,
+		disabled,
+		fullWidth,
+		size = 'm',
+		...otherProps
+	} = props;
 
 	const mods: Mods = {
 		[cls.square]: square,
@@ -33,7 +47,12 @@ export const Button = memo((props: ButtonProps) => {
 	};
 
 	return (
-		<button type="button" className={classNames(cls.Button, mods, [className, cls[variant], cls[size]])} disabled={disabled} {...otherProps}>
+		<button
+			type="button"
+			className={classNames(cls.Button, mods, [className, cls[color], cls[variant], cls[size]])}
+			disabled={disabled}
+			{...otherProps}
+		>
 			<div className={cls.addonLeft}>{addonLeft && addonLeft}</div>
 			{children}
 			<div className={cls.addonRight}>{addonRight && addonRight}</div>
